470c5ccffe3b4c4cc2b7be0d20b5ade11bb69d42
bekousek <155663583+bekousek@users.noreply.github.com>
2025-04-19 00:46:29 +0200

    Add files via upload

---
+++ driver.ts
@@ -0,0 +1,15 @@
+
+/**
+ * mBitBeam - vlastní ovladač pro PCA9685 a další funkce
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace PCA9685 {
+
+    /**
+     * Testovací funkce pro ověření funkčnosti rozšíření
+     */
+    //% block
+    export function testDriver(): void {
+        serial.writeLine("Rozšíření mBitBeam-PCA9685 bylo úspěšně načteno.")
+    }
+}
