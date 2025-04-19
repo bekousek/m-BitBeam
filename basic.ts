573ea83eccf0cdf2f5cf92692f0aea51379048ac
bekousek <155663583+bekousek@users.noreply.github.com>
2025-04-19 17:54:33 +0200

    Create basic.ts

---
+++ basic.ts
@@ -0,0 +1,34 @@
+enum MBRole {
+    //% block="ovladač"
+    Controller,
+    //% block="mozek"
+    Brain
+}
+
+//% color=#00979D icon="\uf2db" block="m-Bit základní"
+namespace mbitbasic {
+
+    let currentRole: MBRole = MBRole.Controller
+
+    //% block="nastav roli na %role"
+    //% weight=100
+    export function setRole(role: MBRole): void {
+        currentRole = role
+        radio.setGroup(42)
+        if (role == MBRole.Controller) {
+            radio.setTransmitPower(7)
+        } else {
+            radio.onReceivedString(function (msg: string) {
+                // Zde později napojíme reakce na zprávy
+            })
+        }
+    }
+
+    //% block="pošli testovací zprávu"
+    //% weight=90
+    export function sendTestMessage(): void {
+        if (currentRole == MBRole.Controller) {
+            radio.sendString("test")
+        }
+    }
+}
